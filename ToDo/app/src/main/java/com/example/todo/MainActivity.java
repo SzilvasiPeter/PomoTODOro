8da5ceb965cd26ec76692fd18d0af3f5b400b9d6
SzilvasiPeter <peti.szilvasi95@gmail.com>
2021-03-18 17:49:33 +0100

    EditText with simple text view update.

--- ToDo/app/src/main/java/com/example/todo/MainActivity.java
+++ ToDo/app/src/main/java/com/example/todo/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.todo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnKeyListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText addEditText = (EditText) findViewById(R.id.addtask_editText);
+        addEditText.setOnKeyListener(this);
+    }
+
+    @Override
+    public boolean onKey(View view, int keyCode, KeyEvent event) {
+
+        TextView responseText = (TextView) findViewById(R.id.simpleTask_textview);
+        EditText myEditText = (EditText) view;
+
+        if (keyCode == EditorInfo.IME_ACTION_SEARCH ||
+                keyCode == EditorInfo.IME_ACTION_DONE ||
+                event.getAction() == KeyEvent.ACTION_DOWN &&
+                        event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+            responseText.setText(myEditText.getText());
+
+            return true;
+        }
+        return false; // pass on to other listeners.
     }
 }
