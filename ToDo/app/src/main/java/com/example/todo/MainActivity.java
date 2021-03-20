1be279701f4a5c3c6b74a57d0c59c8c18c8bc2a9
SzilvasiPeter <peti.szilvasi95@gmail.com>
2021-03-20 13:50:07 +0100

    Add Recycleviewer in MainActivity

--- ToDo/app/src/main/java/com/example/todo/MainActivity.java
+++ ToDo/app/src/main/java/com/example/todo/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnKeyListener {
 
@@ -17,6 +22,17 @@ public class MainActivity extends AppCompatActivity implements View.OnKeyListene
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myTasks.add(new Task("1st Task", 1, new Date(System.currentTimeMillis()), false));
+        myTasks.add(new Task("2st Task", 2, new Date(System.currentTimeMillis()), false));
+        myTasks.add(new Task("3st Task", 3, new Date(System.currentTimeMillis()), false));
+
+
+        RecyclerView recyclerView = findViewById(R.id.tasks_recycleview);
+        myAdapter = new TaskListAdapter(this, myTasks);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(myAdapter);
+
         EditText addEditText = (EditText) findViewById(R.id.addtask_editText);
         addEditText.setOnKeyListener(this);
     }
@@ -24,17 +40,24 @@ public class MainActivity extends AppCompatActivity implements View.OnKeyListene
     @Override
     public boolean onKey(View view, int keyCode, KeyEvent event) {
 
-        TextView responseText = (TextView) findViewById(R.id.simpleTask_textview);
+        TextView responseText = findViewById(R.id.simpleTask_textview);
         EditText myEditText = (EditText) view;
 
         if (keyCode == EditorInfo.IME_ACTION_SEARCH ||
                 keyCode == EditorInfo.IME_ACTION_DONE ||
                 event.getAction() == KeyEvent.ACTION_DOWN &&
                         event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+            
             responseText.setText(myEditText.getText());
 
+            myTasks.add(new Task(myEditText.getText().toString(), 0, new Date(System.currentTimeMillis()), false));
+            myAdapter.notifyItemInserted(myTasks.size() - 1);
+            myEditText.setText("");
             return true;
         }
-        return false; // pass on to other listeners.
+        return false;
     }
+
+    private List<Task> myTasks = new ArrayList<>();
+    private TaskListAdapter myAdapter;
 }
