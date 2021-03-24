0e9fb0374be882d0805acc9e57e7e0073a925eb5
SzilvasiPeter <peti.szilvasi95@gmail.com>
2021-03-24 20:27:07 +0100

    Timer activity - numberpicker

--- ToDo/app/src/main/java/com/example/todo/activities/TimerActivity.java
+++ ToDo/app/src/main/java/com/example/todo/activities/TimerActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import com.example.todo.R;
@@ -21,11 +22,23 @@ public class TimerActivity extends AppCompatActivity {
 
         myTimerTextView = findViewById(R.id.timer_textView);
         myTimerButton = findViewById(R.id.timer_button);
+
+        String[] timerTimes = new String[60];
+        for (int i = 0; i < 60; i++){
+            timerTimes[i] = String.valueOf(i+1);
+        }
+
+        myTimerNumberPicker = findViewById(R.id.timer_numberPicker);
+        myTimerNumberPicker.setMinValue(0);
+        myTimerNumberPicker.setMaxValue(60);
+        myTimerNumberPicker.setDisplayedValues(timerTimes);
+        myTimerNumberPicker.setOnValueChangedListener((numberPicker, i, i1) -> myPickedNumber = numberPicker.getValue());
     }
 
     public void startTimer(View view) {
         myTimerButton.setEnabled(false);
-        new CountDownTimer(125000, 1000) {
+        myTimerNumberPicker.setVisibility(View.INVISIBLE);
+        new CountDownTimer(myPickedNumber*1000, 1000) {
 
             public void onTick(long millisUntilFinished) {
                 long seconds = 0;
@@ -42,10 +55,13 @@ public class TimerActivity extends AppCompatActivity {
             public void onFinish() {
                 myTimerTextView.setText("Done!");
                 myTimerButton.setEnabled(true);
+                myTimerNumberPicker.setVisibility(View.VISIBLE);
             }
         }.start();
     }
 
     private TextView myTimerTextView;
     private Button myTimerButton;
+    private NumberPicker myTimerNumberPicker;
+    private int myPickedNumber;
 }
